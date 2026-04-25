fe1e8ce5d4221781ca8aa4cd576676d894f4eaa9
Dmitriy <mr.dima337@gmail.com>
2026-04-25 23:35:58 +0300

    finished lesson JS-03 sprint-01

--- js-lessons/sprint-01/lesson-03/scripts.js
+++ js-lessons/sprint-01/lesson-03/scripts.js
@@ -10,7 +10,7 @@
 // numbers.__proto__ => ссылка на Array.prototype
 // numbers.__proto__.push.call(numbers, 200)
 
-const numbers = new Array.prototype.constructor(10, 20, 30);
+const numbers = new Array.prototype.constructor(1, 3, 10);
 
 // Array.isArray() => статический метод
 
@@ -18,9 +18,82 @@ const numbers = new Array.prototype.constructor(10, 20, 30);
 class Pet {
     constructor(name) {
         this.name = name
-        this.sayHi = 'Hi from pet'
     }
+    sayHi() {
+        console.log(`hello from ${this.name}`)
+    }
+
 
 }
 
-const cat = new Pet('Majorka')
+const cat = new Pet('Majorka');
+
+Array.prototype.push = function () {
+    alert()
+}
+
+Array.prototype.find = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i]) === true) {
+            return this[i]
+        }
+    }
+    return undefined;
+}
+
+numbers.find(n => n % 2 === 0)
+
+// контекст вызова (так как получает свое значение только в момент вызова)
+// контекст - это объект
+
+Array.prototype.map = function (callback) {
+    const newArr = new Array();
+    for (let i = 0; i < this.length; i++) {
+        newArr[i] = callback(this[i]);
+    }
+    return newArr;
+}
+
+Array.prototype.filter = function (callback) {
+    const newArr = new Array();
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i])) {
+            newArr[newArr.length] = this[i]
+        }
+    }
+
+    return newArr;
+}
+
+
+const test = [23, 24, 33, 12, 56, 23, 11, 55, 4, 7, 7, 7, 8, 3, 2, 24];
+
+const result = {
+    1: 5,
+    7: 3,
+}
+
+test.reduce((acc, el) => {
+    if (acc[el] === undefined) {
+        acc[el] = 1;
+    } else {
+        acc[el] = acc[el] + 1
+    }
+    return acc
+}, {})
+
+
+numbers.reduce((acc, el)=>{
+    acc = acc + el;
+    return acc;
+}, 0);
+
+
+Array.prototype.reduce = function(callback, accumulator) {
+    for (let i = 0; i < this.length; i++) {
+       accumulator = callback(accumulator, this[i])
+        
+    }
+
+    return accumulator;
+}
