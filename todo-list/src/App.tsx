4e40ef35e909dd0d308cf3a45a08f3e18b4a7005
Dmitriy <mr.dima337@gmail.com>
2026-05-13 09:28:23 +0300

    refactor: removed comments

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -21,14 +21,6 @@ export type TodoListType = {
   filter: FilterValues;
 };
 
-// const initialState: Task[] = [
-//   { id: v1(), title: "HTML&CSS", isDone: true },
-//   { id: v1(), title: "JS", isDone: true },
-//   { id: v1(), title: "ReactJS", isDone: false },
-//   { id: v1(), title: "Redux", isDone: false },
-//   { id: v1(), title: "Typescript", isDone: false },
-//   { id: v1(), title: "RTK query", isDone: false },
-// ];
 
 export const App = () => {
   const todolistId1 = v1();
@@ -51,67 +43,25 @@ export const App = () => {
     ],
   });
 
-  // const [tasks, setTasks] = useState(initialState);
-  // // const [filter, setFilter] = useState<FilterValues>("all");
-  // const [todolists, setTodolists] = useState<TodoListType[]>([
-  //   { id: v1(), title: "What to learn", filter: "all" },
-  //   { id: v1(), title: "What to buy", filter: "all" },
-  // ]);
-
-
-  
-  // const deleteTask = (taskId: Task["id"]) => {
-  //   const updateTasks = tasks.filter((task) => task.id !== taskId);
-  //   setTasks(updateTasks);
-  // };
-
   const deleteTask = (todolistId: string, taskId: string) => {
-    const newTasks = {
+    setTasks({
       ...tasks,
-      [todolistId]: tasks[todolistId].filter((el) => el.id !== taskId),
-    };
-    setTasks(newTasks);
-
-    // const tasksForHandling = tasks[todolistId];
-    // const updatedTasks =  tasksForHandling.filter(el => el.id != taskId);
-    // setTasks({...tasks, [todolistId]: updatedTasks})
+      [todolistId]: tasks[todolistId].filter((task) => task.id !== taskId),
+    });
   };
 
   const deleteTodoList = (todolistId: string) => {
     setTodolists(todolists.filter((todolist) => todolist.id !== todolistId));
+
     delete tasks[todolistId];
     setTasks({ ...tasks });
   };
 
   const handleFilterTasks = (todolistId: string, filter: FilterValues) => {
     setTodolists(
-      todolists.map((todolist) =>
-        todolist.id === todolistId ? { ...todolist, filter } : todolist,
-      ),
+      todolists.map((el) => (el.id === todolistId ? { ...el, filter } : el)),
     );
   };
-
-  // const createTask = (title: Task["title"]) => {
-  //   const newTask = { id: v1(), title, isDone: false };
-  //   setTasks([newTask, ...tasks]);
-  // };
-
-  // const createTask = (todolistId: string, title: string) => {
-  //   /** Формируем новую таску: */
-  //   const newTask = {id: v1(), title, isDone: false}
-  //   /** Берем таски тудулиста по его `id`: */
-  //   const todolistTasks = tasks[todolistId]
-  //   /** Перезаписываем массив тасок нужного тудулиста, добавляя в начало новую таску: */
-  //   tasks[todolistId] = [newTask, ...todolistTasks]
-  //   /** Устанавливаем в state копию объекта, чтобы React отреагировал перерисовкой: */
-  //   setTasks({ ...tasks })
-  // }
-
-  // const createTask = (todolistId: string, title: string) => {
-  //   const newTask = {id: v1(), title, isDone: false}
-  //   const newTasks = { ...tasks, [todolistId]: [newTask, ...tasks[todolistId]] }
-  //   setTasks(newTasks)
-  // }
 
   const createTask = (todolistId: string, title: string) => {
     const newTask = { id: v1(), title, isDone: false };
@@ -119,25 +69,17 @@ export const App = () => {
     setTasks({ ...tasks, [todolistId]: [newTask, ...tasks[todolistId]] });
   };
 
-  // const changeTaskStatus = (taskId: Task["id"], isDone: Task["isDone"]) => {
-  //   const updatedTasks: Task[] = tasks.map((task) =>
-  //     task.id === taskId ? { ...task, isDone } : task,
-  //   );
-  //   setTasks(updatedTasks);
-  // };
-
   const changeTaskStatus = (
     todolistId: string,
     taskId: string,
     isDone: boolean,
   ) => {
-    const newTasks = {
+    setTasks({
       ...tasks,
       [todolistId]: tasks[todolistId].map((task) =>
         task.id == taskId ? { ...task, isDone } : task,
       ),
-    };
-    setTasks(newTasks);
+    });
   };
 
   const getFilteredTasks = (tasks: Task[], filter: FilterValues) => {
@@ -151,7 +93,6 @@ export const App = () => {
     }
     return filteredTasks;
   };
-
 
   return (
     <div className="app">
