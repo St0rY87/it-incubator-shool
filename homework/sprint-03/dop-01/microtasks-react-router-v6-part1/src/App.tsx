b12e3cd817d2c6ceca5bdfd824325a141c4ebf03
Dmitriy <mr.dima337@gmail.com>
2026-07-05 20:45:08 +0300

    feat sprint-03: added nav menu in dop-01

--- homework/sprint-03/dop-01/microtasks-react-router-v6-part1/src/App.tsx
+++ homework/sprint-03/dop-01/microtasks-react-router-v6-part1/src/App.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
 import styles from "./components/Site.module.css";
 import { PageOne } from "./components/pages/PageOne";
 import { PageThree } from "./components/pages/PageThree";
@@ -7,26 +13,38 @@ import { Error404 } from "./components/pages/Error404";
 
 export const App = () => {
   return (
-    <div>
-      <div className={styles.header}>
-        <h1>HEADER</h1>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.nav}>Здесь будет навигация</div>
-        <div className={styles.content}>
-          <BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <div className={styles.header}>
+          <h1>HEADER</h1>
+        </div>
+        <div className={styles.body}>
+          <div className={styles.nav}>
+            <ul>
+              <li>
+                <NavLink to={"/pageOne"}>Page 1</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/pageTwo"}>Page 2</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/pageThree"}>Page 3</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.content}>
             <Routes>
-              <Route path="/" element={<Navigate to='/pageOne'/>} />
+              <Route path="/" element={<Navigate to="/pageOne" />} />
               <Route path="/pageOne" element={<PageOne />} />
               <Route path="/pageTwo" element={<PageTwo />} />
               <Route path="/pageThree" element={<PageThree />} />
               <Route path="/error404" element={<Error404 />} />
-              <Route path="*" element={<Navigate to='/error404'/>} />
+              <Route path="*" element={<Navigate to="/error404" />} />
             </Routes>
-          </BrowserRouter>
+          </div>
         </div>
+        <div className={styles.footer}>abibas 2023</div>
       </div>
-      <div className={styles.footer}>abibas 2023</div>
-    </div>
+    </BrowserRouter>
   );
 };
