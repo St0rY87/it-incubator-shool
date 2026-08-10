04050a95d8a9b5c81a82409ba412c3dfe49ac650
Dmitriy <mr.dima337@gmail.com>
2026-08-10 12:10:46 +0300

    init: sprint-03 init app

---
+++ todolist-09-01/src/model/tasks-reducer.ts
@@ -0,0 +1,67 @@
+import { createAction, createReducer, nanoid } from "@reduxjs/toolkit";
+import type { TasksState } from "../app/App";
+import { createTodolistAC, deleteTodolistAC } from "./todolists-reducer";
+
+export const deleteTaskAC = createAction<{
+  todolistId: string;
+  taskId: string;
+}>("tasks/deleteTask");
+
+export const createTaskAC = createAction<{
+  todolistId: string;
+  title: string;
+}>("tasks/createTask");
+
+export const changeTaskStatusAC = createAction<{
+  todolistId: string;
+  taskId: string;
+  isDone: boolean;
+}>("tasks/changeTaskStatus");
+
+export const changeTaskTitleAC = createAction<{
+  todolistId: string;
+  taskId: string;
+  title: string;
+}>("tasks/changeTaskTitle");
+
+const initialState: TasksState = {};
+
+export const tasksReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(deleteTodolistAC, (state, action) => {
+      delete state[action.payload.id];
+    })
+    .addCase(createTodolistAC, (state, action) => {
+      state[action.payload.id] = [];
+    })
+    .addCase(deleteTaskAC, (state, action) => {
+      const index = state[action.payload.todolistId].findIndex(
+        (task) => task.id === action.payload.taskId,
+      );
+      if (index !== -1) state[action.payload.todolistId].splice(index, 1);
+    })
+    .addCase(createTaskAC, (state, action) => {
+      console.log(action);
+      state[action.payload.todolistId].push(
+        {
+        title: action.payload.title,
+        isDone: false,
+        id: nanoid(),
+      }
+      )
+    })
+    .addCase(changeTaskStatusAC, (state, action) => {
+      const index = state[action.payload.todolistId].findIndex(
+        (task) => task.id === action.payload.taskId,
+      );
+      if (index !== -1)
+        state[action.payload.todolistId][index].isDone = action.payload.isDone;
+    })
+    .addCase(changeTaskTitleAC, (state, action) => {
+      const index = state[action.payload.todolistId].findIndex(
+        (task) => task.id === action.payload.taskId,
+      );
+      if (index !== -1)
+        state[action.payload.todolistId][index].title = action.payload.title;
+    });
+});
